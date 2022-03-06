e1f34ca4d2f5ac47e35dc5d8036b4ef59129bfa4
Drayton80 <drayton80@hotmail.com>
2022-03-06 18:31:54 -0300

    Using validator in main with one of the pretrained models

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 import preprocessor
 import oldModel as modeler
 import augmentor
+import validator
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-ag", "--augment", required=False, nargs='?', const=False, help="if the data will be augmented or not")
@@ -18,6 +19,8 @@ if(args["all"] or args["preprocess"]):
     preprocessor.run(compress_in='dataset_train.npz')
 if(args["all"] or args["trainmodel"]):
     modeler.setup_and_train('dataset_train.npz')
+
+validator.predict_test_images('trained models/previews 2/step98699_epoch300_model.h5')
 
 #modeler.generate('maps/preprocessed/0/original/17.png', '001620')
 #modeler.generate('maps/original/maps-of-medieval-cities-bologna.jpg', '001620')
