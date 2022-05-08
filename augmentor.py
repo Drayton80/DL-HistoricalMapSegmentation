0b05baf44d6a98bf05b789f8af67e259391207a1
Drayton80 <drayton80@hotmail.com>
2022-05-08 19:43:50 -0300

    Augmented refactors and improvements

--- augmentor.py
+++ augmentor.py
@@ -3,12 +3,12 @@ from typing import List
 from PIL import Image
 from pathlib import Path
 
-from utils import get_maps_source, get_maps_mask
+from utils import get_maps, get_masks
 
 
 def run() -> None:
     # Concatenates the maps's sources and the maps's masks
-    maps_all: List[Image.Image] = [*get_maps_source(), *get_maps_mask()]
+    maps_all: List[Image.Image] = [*get_maps(), *get_masks()]
     augmented_folder = "./maps/augmented/"
     Path(augmented_folder).mkdir(parents=True, exist_ok=True)
     
@@ -20,26 +20,24 @@ def run() -> None:
         # Getting map's size:
         map_width: int = map_img.size[0]
         map_height: int = map_img.size[1]
+
+        images_extension = '.jpg'
+        # Removing the Alpha channel:
+        map_img = map_img.convert('RGB')
         # Saving the source:
-        map_img.save(map_name + '.png')
+        map_img.save(map_name + images_extension)
         # Half-size
         half_suffix = '-halfSize'
         map_half = map_img.resize((int(map_width/2), int(map_height/2)))
-        map_half.save(map_name + half_suffix + '.png')
-        # Double size:
-        double_suffix = '-doubleSize'
-        map_double = map_img.resize((int(map_width*2), int(map_height*2)))
-        map_double.save(map_name + double_suffix + '.png')
+        map_half.save(map_name + half_suffix + images_extension)
         # Rotation
         rotate_suffix = '-rotate180'
-        map_img.rotate(180).save(map_name + rotate_suffix + '.png')
-        map_half.rotate(180).save(map_name + half_suffix + rotate_suffix + '.png')
-        map_double.rotate(180).save(map_name + double_suffix + rotate_suffix + '.png')
+        map_img.rotate(180).save(map_name + rotate_suffix + images_extension)
+        map_half.rotate(180).save(map_name + half_suffix + rotate_suffix + images_extension)
         # Horizontal Flip
         flip_suffix = '-flipHorizontal'
-        map_img.transpose(Image.FLIP_LEFT_RIGHT).save(map_name + flip_suffix + '.png')
-        map_half.transpose(Image.FLIP_LEFT_RIGHT).save(map_name + half_suffix + flip_suffix + '.png')
-        map_double.transpose(Image.FLIP_LEFT_RIGHT).save(map_name + double_suffix + flip_suffix + '.png')
+        map_img.transpose(Image.FLIP_LEFT_RIGHT).save(map_name + flip_suffix + images_extension)
+        map_half.transpose(Image.FLIP_LEFT_RIGHT).save(map_name + half_suffix + flip_suffix + images_extension)
         
 
 
