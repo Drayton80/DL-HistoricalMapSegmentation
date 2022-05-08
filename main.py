d7abb5d92f238e9fe09c937537aea2b1bd830325
Drayton80 <drayton80@hotmail.com>
2022-05-08 13:45:11 -0300

    Main args definition fix

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ import augmentor
 import tester
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-ag", "--augment", required=False, nargs='?', const=False, help="if the data will be augmented or not")
-ap.add_argument("-p", "--preprocess", required=False, nargs='?', const=False, help="if the data will be preprocessed or not")
-ap.add_argument("-tr", "--train", required=False, nargs='?', const=False, help="if the model will train or not")
-ap.add_argument("-te", "--test", required=False, nargs='?', const=False, help="if the model will run the test validation or not")
-ap.add_argument("-a", "--all", required=False, nargs='?', const=False, help="if will run all above")
+ap.add_argument("-ag", "--augment", required=False, nargs='?', const=True, help="if the data will be augmented or not")
+ap.add_argument("-p", "--preprocess", required=False, nargs='?', const=True, help="if the data will be preprocessed or not")
+ap.add_argument("-tr", "--train", required=False, nargs='?', const=True, help="if the model will train or not")
+ap.add_argument("-te", "--test", required=False, nargs='?', const=True, help="if the model will run the test validation or not")
+ap.add_argument("-a", "--all", required=False, nargs='?', const=True, help="if will run all above")
 
 args = vars(ap.parse_args())
 
