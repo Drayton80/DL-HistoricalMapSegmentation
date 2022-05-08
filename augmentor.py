737de808e1e05e57d1090972f271c5a1bf493615
Drayton80 <drayton80@hotmail.com>
2022-05-08 15:36:01 -0300

    Add augmented folder treatment

--- augmentor.py
+++ augmentor.py
@@ -1,18 +1,23 @@
 import re
 from typing import List
 from PIL import Image
+from pathlib import Path
 
 from utils import get_maps_source, get_maps_mask
 
 
 def run() -> None:
     # Concatenates the maps's sources and the maps's masks
-    maps_all: List[Image.Image] = [*get_maps_source(), *get_maps_mask()] 
+    maps_all: List[Image.Image] = [*get_maps_source(), *get_maps_mask()]
+    augmented_folder = "./maps/augmented/"
+    Path(augmented_folder).mkdir(parents=True, exist_ok=True)
     
     # Apply the augmentation for each map in the pair of mask and source
     for map_img in maps_all:
         # Path of the map plus its name without its file type
-        map_name: str = re.sub(r'\.(png|jpg)$', '', map_img.filename)
+        map_name: str = re.sub(r'\.(png|jpg)$', '', map_img.filename).replace('maps/training/', augmented_folder)
+        print('> Augmenting map: ' + map_name)
+        # Getting map's size:
         map_width: int = map_img.size[0]
         map_height: int = map_img.size[1]
         # Resize
