0c43f55accf3e7a04917381dbf790c26a1b46dc1
Drayton80 <drayton80@hotmail.com>
2022-05-08 15:40:39 -0300

    Small refactor in train name

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import tester
 ap = argparse.ArgumentParser()
 ap.add_argument("-ag", "--augment", required=False, nargs='?', const=True, help="if the data will be augmented or not")
 ap.add_argument("-p", "--preprocess", required=False, nargs='?', const=True, help="if the data will be preprocessed or not")
-ap.add_argument("-tr", "--train", required=False, nargs='?', const=True, help="if the model will train or not")
+ap.add_argument("-tr", "--training", required=False, nargs='?', const=True, help="if the model will be training or not")
 ap.add_argument("-te", "--test", required=False, nargs='?', const=True, help="if the model will run the test validation or not")
 ap.add_argument("-a", "--all", required=False, nargs='?', const=True, help="if will run all above")
 
@@ -19,9 +19,9 @@ args = vars(ap.parse_args())
 if(args["all"] or args["augment"]):
     augmentor.run()
 if(args["all"] or args["preprocess"]):
-    preprocessor.run(compress_in='dataset_train.npz')
-if(args["all"] or args["train"]):
-    trainer.run('dataset_train.npz')
+    preprocessor.run(compress_in='dataset_training.npz')
+if(args["all"] or args["training"]):
+    trainer.run('dataset_training.npz')
 if(args["all"] or args["test"]):
     latest_model:str = max(glob.glob('./trained models/*.h5'), key=os.path.getctime)
     tester.run('trained models/' + latest_model)
